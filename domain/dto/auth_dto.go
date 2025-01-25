a20133724b89c44354ef8a389d3166595dd4d55b
nathakusuma <nathakusuma07@gmail.com>
2025-01-25 16:38:57 +0700

    feat(auth): add check otp register user

--- domain/dto/auth_dto.go
+++ domain/dto/auth_dto.go
@@ -3,3 +3,8 @@ package dto
 type RequestOTPRegisterUserRequest struct {
 	Email string `json:"email" validate:"required,email,max=320"`
 }
+
+type CheckOTPRegisterUserRequest struct {
+	Email string `json:"email" validate:"required,email"`
+	OTP   string `json:"otp" validate:"required"`
+}

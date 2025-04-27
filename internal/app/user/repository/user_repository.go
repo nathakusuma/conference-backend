a875e8471dd893f22b792efb1e541a52ef2f02e8
nathakusuma <nathakusuma07@gmail.com>
2025-04-27 16:04:00 +0700

    feat(user): add create user

---
+++ internal/app/user/repository/user_repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/nathakusuma/astungkara/domain/contract"
+	"github.com/nathakusuma/astungkara/domain/entity"
+)
+
+type userRepository struct {
+	conn *sqlx.DB
+}
+
+func NewUserRepository(conn *sqlx.DB) contract.IUserRepository {
+	return &userRepository{
+		conn: conn,
+	}
+}
+
+func (r *userRepository) CreateUser(ctx context.Context, user *entity.User) error {
+	return r.createUser(ctx, r.conn, user)
+}
+
+func (r *userRepository) createUser(ctx context.Context, tx sqlx.ExtContext, user *entity.User) error {
+	_, err := sqlx.NamedExecContext(
+		ctx,
+		tx,
+		`INSERT INTO users (
+                   id, name, password_hash, role, email, auth_method
+                   ) VALUES (:id, :name, :password_hash, :role, :email, :auth_method)`,
+		user,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

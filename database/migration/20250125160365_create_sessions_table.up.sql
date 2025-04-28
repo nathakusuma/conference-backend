4b25015f4f5ea6df0637d330257eabf523d2daa2
nathakusuma <nathakusuma07@gmail.com>
2025-04-28 13:24:00 +0700

    fix(auth): change session primary key to token

--- database/migration/20250125160365_create_sessions_table.up.sql
+++ database/migration/20250125160365_create_sessions_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE sessions
 (
-    user_id    UUID PRIMARY KEY REFERENCES users (id) ON DELETE CASCADE,
-    token      CHAR(64)  NOT NULL,
+    token   CHAR(32) PRIMARY KEY,
+    user_id UUID REFERENCES users (id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP NOT NULL
 );
 
 CREATE INDEX sessions_expires_at_idx ON sessions (expires_at);
+CREATE UNIQUE INDEX sessions_user_id_key ON sessions (user_id);
--- domain/entity/session_entity.go
+++ domain/entity/session_entity.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Session struct {
-	UserID    uuid.UUID `json:"user_id" db:"user_id"`
 	Token     string    `json:"token"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	ExpiresAt time.Time `json:"expires_at" db:"expires_at"`
 }
